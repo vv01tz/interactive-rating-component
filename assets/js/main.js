ef614df09d3f64c0a7c2d7ff074cf2366222797f
arnovolts <thebluehermit@protonmail.com>
2022-05-13 13:11:34 +0200

    Add logic for hide initial state and showing new state

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,10 +4,14 @@ window.addEventListener("DOMContentLoaded", function() {
       event.preventDefault();
       let ratingDisplay = document.querySelector("#user-rating-display");
       let ratings =  ratingForm.elements["rating"]; 
+      let thankYouCard = document.querySelector(".card-thank-you");
+      let card = document.querySelector(".card");
 
       for (let i = 0; i < ratings.length; i++) {
           if (ratings[i].checked){
+              card.style.display = "none";
               ratingDisplay.textContent = ratings[i].value;
+              thankYouCard.style.display = "block";
               break;
           }
       }
